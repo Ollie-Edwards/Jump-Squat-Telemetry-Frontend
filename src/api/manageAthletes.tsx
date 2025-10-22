23e06bbe104aee5991d32d19f3f157ffb42c6dbf
Ollie-Edwards <ollie3edwards@gmail.com>
2025-10-22 19:53:43 +0100

    developing view single athlete data dropdowns, to select the athlete and the corresponding 'trial'

--- src/api/manageAthletes.tsx
+++ src/api/manageAthletes.tsx
@@ -4,3 +4,8 @@ export const getAthletes = async () => {
   const { data } = await api.get('/athlete/all');
   return data
 };
+
+export const getAllAthleteTrials = async (name : string) => {
+  const { data } = await api.get("/trial/"+name);
+  return data
+};
--- src/pages/SingleAthleteData.tsx
+++ src/pages/SingleAthleteData.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
-import { getAthletes } from "../api/manageAthletes"
+import { getAthletes, getAllAthleteTrials } from "../api/manageAthletes"
 
 import {
   Select,
@@ -13,40 +14,86 @@ import {
 } from "@/components/ui/select"
 
 function SingleAthleteData() {
-  const [data, setData] = useState([]);
+  const [athletes, setAthletes] = useState([]);
   const [loading, setLoading] = useState<boolean>(true);
 
-   const getData = async () => {
+  const [athleteTrials, setAthleteTrials] = useState([]);
+  const [selectedAthlete, setSelectedAthlete] = useState("");
+
+   const getAllAthletes = async () => {
     setLoading(true);
     try {
       const response = await getAthletes();
-      setData(response);
+      setAthletes(response);
       console.log(response);
     } finally {
       setLoading(false);
     }
   };
 
+   const updateAthleteTrials = async (athleteName : string) => {
+    try {
+      console.log("trying " + athleteName)
+      const response = await getAllAthleteTrials(athleteName);
+      console.log("get athlete trials")
+      console.log(response)
+      setAthleteTrials(response);
+    } finally { }
+  };
+
   useEffect(() => {
-    getData()
+    getAllAthletes()
   }, []);
+
+  useEffect(() => {
+    if (selectedAthlete){
+      console.log(selectedAthlete)
+    }
+  }, [selectedAthlete]);
+
+  const handleChange = (athleteName : string) => {
+    setSelectedAthlete(athleteName)
+    updateAthleteTrials(athleteName)
+  }
 
   return (
     <div className="w-150 mx-auto">
       <h1 className="text-3xl text-center my-10">Select Athlete</h1>
-      <Select>
+      <Select onValueChange={(athleteName) => handleChange(athleteName)}>
         <SelectTrigger className="w-full">
           <SelectValue placeholder="Athlete Selection" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
             <SelectLabel>Athlete</SelectLabel>
-            {data.map((name) => (
+            {athletes.map((name) => (
               <SelectItem value={name}>{name}</SelectItem>
             ))}
           </SelectGroup>
         </SelectContent>
       </Select>
+
+      { selectedAthlete ? 
+        <>
+          <h1 className="text-3xl text-center my-10">Select Effort</h1>
+          <Select onValueChange={() =>{}}>
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder="Athlete Selection" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Trial</SelectLabel>
+                {athleteTrials.map((trial) => (
+                  <SelectItem value={trial.trial_id}>{ dayjs(trial.trial_date).format("DD/MM/YY HH:mm A") } - {trial.num_jumps} Jumps</SelectItem>
+                ))}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </>
+        :
+        <></>
+      }
+
     </div>
   )
 }
