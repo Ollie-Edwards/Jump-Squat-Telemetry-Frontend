3a53e7dff7467c3268aca72235eed2d463dc10aa
Ollie-Edwards <ollie3edwards@gmail.com>
2025-11-09 14:56:34 +0000

    added relevent attributes to the control form, and passed these to the popup. Allowed popup to be disabled or enabled externally, and resets on open

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,25 +3,23 @@ import { beginRecording, createTrial } from "../api/manageSensor.ts"
 import { Button } from "@/components/ui/button"
 import { ImCross } from "react-icons/im";
 
-export function Countdown() {
+export interface CountdownProps {
+  active: boolean;
+  setActive: React.Dispatch<React.SetStateAction<boolean>>;
+  athleteID:  number;
+  restTime: number;
+  jumpTime: number;
+  maxJumpNum: number;
+  barWeight: number;
+}
+
+export function Countdown({active, setActive, athleteID, restTime, jumpTime, maxJumpNum, barWeight}: CountdownProps) {
   const [timeLeft, setTimeLeft] = useState(10);
   const [trialNumber, setTrialNumber] = useState(1);
   const [rest, setRest] = useState(true);
-  const [trialID, setTrialID] = useState(null);
 
-  const [active, setActive] = useState(true);
-
-  const restTime = 10;
-  const jumpTime = 5;
-  const maxTrial = 3;
-
-  function enable(){
-    setActive(true)
-  } 
-  
-  function disable(){
-    setActive(false)
-  }
+  type TrialID = number | null; // or number | null
+  const [trialID, setTrialID] = useState<TrialID>(null);
 
   function incrementTrialNumber(){
     setTrialNumber(trialNumber+1)
@@ -31,16 +29,27 @@ export function Countdown() {
     setRest(!rest)
   }
 
+  function resetData(){
+    setTimeLeft(10)
+    setTrialNumber(1)
+    setRest(true)    
+  }
+
   function onComplete(){
     console.log("completed")
-    if (trialNumber <= maxTrial){
+    if (trialNumber <= maxJumpNum){
 
       toggleRest()
 
       if (rest){
         // send jump command
         setTimeLeft(jumpTime)
-        beginRecording(jumpTime, trialID, trialNumber)
+        if (trialID !== null) {
+          beginRecording(jumpTime, trialID, trialNumber)
+        }
+        else{
+          console.log("recording failed, trialID is null")
+        }
       }
       else{
         incrementTrialNumber()
@@ -50,15 +59,18 @@ export function Countdown() {
   }
 
   async function makeTrial(){
-    const response = await createTrial(1, 65, 3)
+    const response = await createTrial(athleteID, barWeight, maxJumpNum)
     console.log(`created trial with id ${response}`)
     setTrialID(response)
   }
 
   // /trial/create
   useEffect(() => {
-    makeTrial()
-  }, []);
+    if (active){
+      resetData()
+      makeTrial()
+    }
+  }, [active]);
 
   useEffect(() => {
     if (timeLeft <= 0) {
@@ -78,10 +90,10 @@ export function Countdown() {
   return (
     <div className={`fixed inset-0 flex items-center justify-center bg-black/60 z-50 ${active ? "" : "hidden"}`} >
       <div className={`flex flex-col items-center p-6 text-center w-150 h-100 font-semibold text-white rounded-2xl shadow-lg ${rest ? "bg-red-500" : "bg-green-500"}`}>
-        <Button className="ml-auto bg-black-500 bg-opacity-50" variant="secondary" size="icon" onClick={disable}>
+        <Button className="ml-auto bg-black-500 bg-opacity-50" variant="secondary" size="icon" onClick={() => setActive(false)}>
           <ImCross />
         </Button>
-        { trialNumber <= maxTrial ? 
+        { trialNumber <= maxJumpNum ? 
           <>
             <p className="text-xl">Trial Number: {trialNumber}</p>
 
@@ -89,6 +101,7 @@ export function Countdown() {
               {timeLeft > 0 && rest ?`Jump in ${timeLeft} second${timeLeft !== 1 ? "s" : ""}` : ""}
               {timeLeft > 0 && !rest ? `JUMP (${timeLeft}s)` : ""}
             </h1>
+            <p className="text-xs">athleteID: {athleteID}, restTime: {restTime}, jumpTime: {jumpTime}, maxJumpNum: {maxJumpNum}, barWeight:{barWeight}kg</p>
           </>
           : <h1 className="text-4xl my-auto">Success</h1>} 
       </div>
